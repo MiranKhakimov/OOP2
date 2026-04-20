ab76e0a5f861b13e40e3bdbf61b4f3646780aa89
MiranKhakimov <perdarinus@gmail.com>
2026-04-20 08:04:24 +0500

    Point class with Constructors and Destructor

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Point {
+public:
+	double x;
+	double y;
+	Point() {
+		x = 0;
+		y = 0;
+	}
+
+	Point(double x, double y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	Point(const Point& other) {
+		x = other.x;
+		y = other.y;
+	}
+
+	~Point() {
+		printf("Point destructed");
+	}
+};
